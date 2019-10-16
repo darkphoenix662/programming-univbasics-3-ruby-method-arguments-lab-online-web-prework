9fc50f97ebd97da9dc76a4e11b8c7b6bfce6bdba
Sean Good <tree662@protonmail.com>
2019-10-16 20:56:09 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,31 +1,17 @@
-name = Sean
-language = Ruby
-
-def introduction
-  puts "Hi my name is {name}."
+def introduction (name)
+  puts "Hi my name is #{name}."
 end
 
-def introduction_with_language 
+def introduction_with_language (name, language)
   puts "Hi my name is {name} and I am learning to program in {language}"
 end
 
-def introduction_with_language_optional
+def introduction_with_language_optional (name, language)
   puts "Hi my name is {name} and I am learning to program in {language}."
 end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+introdution (Sean)
+introduction_with_language
 
 
 
