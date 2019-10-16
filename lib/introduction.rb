1e0e0d93594e2e95fa7fb0d323832aa7b6936563
Sean Good <tree662@protonmail.com>
2019-10-16 21:06:02 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,7 +10,7 @@ def introduction_with_language_optional (name, language)
   puts "Hi my name is {name} and I am learning to program in {language}."
 end
 
-introdution ("Sean")
+introduction ("Sean")
 introduction_with_language ("Sean", "Ruby")
 introduction_with_language_optional ("Sean", "Ruby")
 
